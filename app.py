689aac06e56b09e7bd4a51c9c64ddbe48f5a44d6
Yessenia H <gabyyeess@gmail.com>
2026-05-01 17:31:49 -0600

    Ajuste nombre grafico

--- app.py
+++ app.py
@@ -22,38 +22,36 @@ if chart_type == 'Histograma':
         'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
 
     # Crear una figura de Plotly
-    fig = go.Figure()
+    fig1 = go.Figure()
 
     # Agregar un gráfico de dispersión para visualizar la relación entre el año y el precio
-    fig.add_trace(go.Scatter(
+    fig1.add_trace(go.Scatter(
         x=car_data['model_year'], y=car_data['price'], mode='markers', name='Price vs Year'))
 
     # Configurar el diseño del gráfico
-    fig.update_layout(title='Price vs Year of Vehicles',
-                      xaxis_title='Year',
-                      yaxis_title='Price',
-                      showlegend=True)
-
-    st.plotly_chart(fig, use_container_width=True)
+    fig1.update_layout(title='Price vs Year of Vehicles',
+                       xaxis_title='Year',
+                       yaxis_title='Price',
+                       showlegend=True)
 
     # Mostrar el gráfico en Streamlit
-    st.plotly_chart(fig)
+    st.plotly_chart(fig1, use_container_width=True)
 elif chart_type == 'Gráfico de dispersión':
     # Escribir un mensaje en la aplicación
     st.write('Creación de un gráfico de dispersión ')
 
     # Crear una figura de Plotly
-    fig = go.Figure()
+    fig2 = go.Figure()
 
     # Agregar un gráfico de dispersión para visualizar la relación entre el año y el precio
-    fig.add_trace(go.Scatter(
+    fig2.add_trace(go.Scatter(
         x=car_data['model_year'], y=car_data['price'], mode='markers', name='Price vs Year'))
 
     # Configurar el diseño del gráfico
-    fig.update_layout(title='Price vs Year of Vehicles',
-                      xaxis_title='Year',
-                      yaxis_title='Price',
-                      showlegend=True)
+    fig2.update_layout(title='Price vs Year of Vehicles',
+                       xaxis_title='Year',
+                       yaxis_title='Price',
+                       showlegend=True)
 
     # Mostrar el gráfico en Streamlit
-    st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig2, use_container_width=True)
